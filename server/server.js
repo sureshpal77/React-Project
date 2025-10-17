206b75acbe402b5d383c614b6ca39ff3d7c4e784
suresh pal <moodsureshpal@gmail.com>
2025-10-18 00:45:00 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://educase-project-backend.vercel.app'
+    'https://educase-project-sigma.vercel.app'
   ],
   credentials: true
 }));
